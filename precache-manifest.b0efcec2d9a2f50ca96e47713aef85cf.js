ad239699ef43018f7b353a62492f6fa5a3dc4483
Stanislav-Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-10-05 18:36:43 +0000

    Deploying to gh-pages from @ Stanislav-Sobolev/password-manager@165211bc49e0dd843636aeaf93481afcfb5e1eee 🚀

---
+++ precache-manifest.b0efcec2d9a2f50ca96e47713aef85cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d65a7778b4b4726e6e27",
+    "url": "/password-manager/static/css/main.5c29444a.chunk.css"
+  },
+  {
+    "revision": "d65a7778b4b4726e6e27",
+    "url": "/password-manager/static/js/main.8fcdb328.chunk.js"
+  },
+  {
+    "revision": "a9ad995287bf51295a51",
+    "url": "/password-manager/static/js/runtime~main.ac3e03fa.js"
+  },
+  {
+    "revision": "6f7c72607cdac7a8b15f",
+    "url": "/password-manager/static/js/2.646bc848.chunk.js"
+  },
+  {
+    "revision": "279ffbe5104508b950ad",
+    "url": "/password-manager/static/js/3.bb78cc89.chunk.js"
+  },
+  {
+    "revision": "98dbf3a67ddc596e7ba1",
+    "url": "/password-manager/static/js/4.2b790792.chunk.js"
+  },
+  {
+    "revision": "1b9b69fffdb2eb2c9de5",
+    "url": "/password-manager/static/js/5.080cffac.chunk.js"
+  },
+  {
+    "revision": "c9aeeb7471afbdd9a0ef",
+    "url": "/password-manager/static/js/6.49e208b5.chunk.js"
+  },
+  {
+    "revision": "1b0760d3d46fd724bc97855352cab66f",
+    "url": "/password-manager/index.html"
+  }
+];
